960c5fceb7c6a2ea0a3765a775d0d6b2b820dc10
Nnamdi <nokeke67@gmail.com>
2026-06-08 17:22:16 -0500

    test3

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,9 +2,9 @@
 
 terraform {
   backend "s3" {
-    bucket = "zen-pharma-terraform-state-andrino1973g"
-    key    = "envs/dev/terraform.tfstate"
+    bucket       = "zen-pharma-terraform-state-andrino1973g"
+    key          = "envs/dev/terraform.tfstate"
     encrypt      = true
-    use_lockfile = true   # S3 native locking
+    use_lockfile = true # S3 native locking
   }
 }
